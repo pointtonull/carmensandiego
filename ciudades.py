624bdebe20556e42b6c1bf7e0cdd0a47cb457aec
mattgaviota <mattgaviota@gmail.com>
2010-07-06 03:26:47 -0300

    sistema de viaje innecesario remodelado

---
+++ ciudades.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+ciudades = {
+    ('Buenos Aires', 'Santiago de Chile'): 1139, 
+    ('Santiago de Chile', 'Nueva York'): 8206,
+    #('Nueva York', 'Buenos Aires'): 8491,
+    #('Buenos Aires', 'Tokio'): 18374,
+    ('Santiago de Chile', 'Tokio'): 17227,
+    ('Nueva York', 'Tokio'): 10858
+         }
