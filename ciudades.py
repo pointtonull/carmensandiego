528aef3e683b355b53c02ae0a6b0fb47fd98d4ad
mattgaviota <mattgaviota@gmail.com>
2010-07-08 00:57:09 -0300

    actualizacion importante

--- ciudades.py
+++ ciudades.py
@@ -2,28 +2,48 @@
 #-*- coding: utf-8 -*-
 
 ciudades = {
-    ('Buenos Aires', 'Santiago de Chile'): 1139, 
-    ('Santiago de Chile', 'Nueva York'): 8206,
-    #('Nueva York', 'Buenos Aires'): 8491,
-    #('Buenos Aires', 'Tokio'): 18374,
-    ('Santiago de Chile', 'Tokio'): 17227,
-    ('Nueva York', 'Tokio'): 10858
+    ('Buenos Aires', 'Rio de Janeiro'): 1960, 
+    ('Rio de Janeiro', 'Londres'): 9240,
+    ('Nueva York', 'Londres'): 5570,
+    ('Buenos Aires', 'Mexico DF'): 7360,
+    ('Rio de Janeiro', 'Beijing'): 17300,
+    ('Nueva York', 'Paris'): 5840,
+    ('Londres', 'Nueva Delhi'): 6700,
+    ('Beijing', 'Nueva Delhi'): 3790,
+    ('Sidney', 'Tokio'): 7780,
+    ('Beijing', 'Tokio'): 2100,
+    ('Paris', 'Tokio'): 9720,
+    ('Mexico DF', 'Paris'): 9200,
+    ('Mexico DF', 'Nueva Delhi'): 14650,
+    ('Buenos Aires', 'El Cairo'): 11790,
+    ('Sidney', 'El Cairo'): 14370,
+    ('Nueva York', 'El Cairo'): 9000
          }
+         
 opciones = {
     '1': 'Buenos Aires',
-    '2': 'Santiago de Chile',
+    '2': 'Rio de Janeiro',
     '3': 'Nueva York',
-    '4': 'Tokio'
+    '4': 'Tokio',
+    '5': 'Londres',
+    '6': 'Paris',
+    '7': 'El Cairo',
+    '8': 'Sidney',
+    '9': 'Nueva Delhi',
+    '10': 'Beijing',
+    '11': 'Mexico DF'
     }
 
 def menu_ciudades():
     print u"""
     Ciudades
     
-        [1] : Buenos Aires
-        [2] : Santiago de Chile
-        [3] : Nueva York
-        [4] : Tokio 
+        [1]  : Buenos Aires     [6]  : Paris       [11] : Mexico DF
+        [2]  : Rio de janeiro   [7]  : El Cairo
+        [3]  : Nueva York       [8]  : Sidney
+        [4]  : Tokio            [9]  : Nueva Delhi
+        [5]  : Londres          [10] : Beijing
+                    
     """
 
 def origen_y_destino(opcion = 0):
--- sistemadeviajes.py
+++ sistemadeviajes.py
@@ -6,99 +6,172 @@
 
 from os import system
 from time import sleep
+from time import ctime, mktime
 from ciudades import ciudades, menu_ciudades, opciones, origen_y_destino 
 
+jugadores = {}
 
-def viajar(origen, destino):
-    '''muestra, si es posible, el viaje entre 2 ciudades 
-    y cuanto dura en horas'''
-    if origen != destino:
-        kilometros = distancia(origen, destino)
-        horas = kilometros / 600
-        sleep(1)
-        if kilometros != -1:
-            print 'Viajando desde %s' %(origen,)
-            for i in xrange(kilometros/100):
-                print '.',
-                #sleep(.1)            
-            print 'Llegamos a %s en %s Horas' %(destino, horas)
+class detective():
+    def __init__(self, lugar, hora):
+        self.lugar_actual = lugar
+        self.hora_actual = hora
+    
+    def viajar(self, origen, destino):
+        '''muestra, si es posible, el viaje entre 2 ciudades 
+        y cuanto dura en horas'''
+        if origen != destino:
+            kilometros = self.distancia(origen, destino)
+            horas = kilometros / 600
+            sleep(1)
+            if kilometros != -1:
+                print 'Viajando desde %s' %(origen,)
+                for i in xrange(kilometros/100):
+                    print '.',
+                    #sleep(.1)            
+                print 'Llegamos a %s en %s Horas' %(destino, horas)
+                self.lugar_actual = destino
+            else:
+                print 'No hay una conexion entre esas dos ciudades'
+                print 'Vea las conexiones y pruebe haciendo escalas'
         else:
-            print 'No hay una conexion entre esas dos ciudades'
-            print 'Vea las conexiones y pruebe haciendo escalas'
-    else:
-        print 'Ya se encuentra en esa ciudad'
+            print 'Ya se encuentra en esa ciudad'
      
-        
-
-def distancia(origen, destino):
-    '''Averigua la distancia en kilometros, si es que existe una conexion
-    directa, entre dos ciudades'''
-    try:
-        return ciudades[(origen, destino)]
-    except KeyError:
+    def distancia(self, origen, destino):
+        '''Averigua la distancia en kilometros, si es que existe una conexion
+        directa, entre dos ciudades'''
         try:
-            return ciudades[(destino, origen)]
+            return ciudades[(origen, destino)]
         except KeyError:
-            return -1
+            try:
+                return ciudades[(destino, origen)]
+            except KeyError:
+                return -1
 
-def menu():
-    '''simple menu con las opciones del programa'''
+    def elegir_viaje(self):
+        system('clear')
+        destino = origen_y_destino(2)
+        self.viajar(self.lugar_actual, destino)
+
+    def ver_conexiones(self):
+        '''dada una ciudad, muestra todas las conexiones directas 
+        con otras ciudades'''
+        system('clear')
+        ciudad = self.lugar_actual
+        claves = ciudades.keys()
+        for clave in claves:
+            if ciudad in clave:
+                print clave
+    
+    def pistas(self):
+        pass
+    
+    def arresto(self):
+        pass
+    
+
+
+def juego_nuevo():
+    system('clear')
     print u"""
-    Menu
-    
-        [1] : Viajar
-        [2] : Ver conexiones
-        [3] : Salir
-    """
-
-def elegir_viaje():
-    system('clear')
-    origen, destino = origen_y_destino()
-    viajar(origen, destino)
-
-def ver_conexiones():
-    '''dada una ciudad, muestra todas las conexiones directas 
-    con otras ciudades'''
-    system('clear')
-    menu_ciudades()
-    opcion = raw_input('Elija una ciudad para ver sus conexiones : ')
-    
-    try:
-        ciudad = opciones[opcion]
-    except KeyError:
-        print 'La opcion no corresponde a una ciudad'
-    
-    claves = ciudades.keys()
-    for clave in claves:
-        if ciudad in clave:
-            print clave
-    
-funciones = {
-    '1': elegir_viaje,
-    '2': ver_conexiones,
-    '3': quit 
-    }    
-    
-
+    +------------------------------+
+    |Cuartel General de la interpol|
+    +------------------------------+
         
+    Lunes 9:00 am
+        
+    Identificate por favor
+    """
+    nombre = raw_input('Nombre : ')
+    jugadores[nombre] = detective('Buenos Aires', ctime())
+    print 'Tu nombre no se encuentra en la base de datos'
+    sleep(1)
+    print ''
+    print 'Tu rango actual es Novato'
+    sleep(3)
+    system('clear')
+    print u"""
+                                *** Noticias ***
+        Tesoro nacional robado en Buenos Aires
+        
+        El botin ha sido identificado como el sable curvo de San Martin.    
+        Un sospechoso de sexo femenino fue visto en la escena del crimen
+        ----------------------------------------------------------------
+        
+        Tu misión:
+            Perseguir al ladron desde Buenos Aires
+            hasta su escondite y arrestarlo. Tienes hasta el domingo
+            a las 23:00 para arrestarlo.
+        ----------------------------------------------------------------
+        
+        Buena suerte novato %s
+    """ %(nombre,)
+    sleep(10)
+    control_central(jugadores[nombre])
+
+
+
+def control_central(novato):
+    system('clear')
+    print u"""
+    
+        %s | %s
+        
+        +-----+ +------+ +------+ +-------+ +-----+
+        |     | |      | |      | |       | |     |
+        | ver | |Viajar| |Pistas| |Arresto| |Salir|
+        |     | |      | |      | |       | |     |
+        +-----+ +------+ +------+ +-------+ +-----+
+          [1]      [2]      [3]      [4]      [5]
+    """ %(novato.lugar_actual, novato.hora_actual)
+    opcion = raw_input('Ingrese opcion : ')
+    while opcion != '5':
+        if opcion == '1':
+            novato.ver_conexiones()
+        elif opcion == '2':
+            novato.elegir_viaje()
+        elif opcion == '3':
+            novato.pistas()
+        elif opcion == '4':
+            novato.arresto()
+        else:
+            print 'opcion fuera de rango'
+            system('clear')
+        print u"""
+            
+            +-----+ +------+ +------+ +-------+ +-----+
+            |     | |      | |      | |       | |     |
+            | ver | |Viajar| |Pistas| |Arresto| |Salir|
+            |     | |      | |      | |       | |     |
+            +-----+ +------+ +------+ +-------+ +-----+
+              [1]      [2]      [3]      [4]      [5]
+        """
+        opcion = raw_input('Ingrese opcion : ')
+ 
+def menu():
+        '''simple menu con las opciones del programa'''
+        print u"""
+        Demo Juanita la Ladrona
+        
+            [1] : Nuevo juego
+            [2] : Salir
+        """    
+
+funciones = {
+    '1': juego_nuevo,
+    '2': quit
+    }    
     
 
 def main():
     system('clear')
     menu()
     opcion = raw_input('Elige opcion : ')
-    while opcion != '3':
+    if opcion != '2':
         try:
             system('clear')
             funciones[opcion]()
         except KeyError:
             print 'Opción fuera de rango'
-    
-        menu()
-        opcion = raw_input('Ingrese opcion: ')         
-        
-    
-        
     
 
 if __name__ == '__main__':
